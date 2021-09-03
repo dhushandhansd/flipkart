cc2f795d2c673a3c3d322a20ad3e6cada6f30e84
Shreedhushandhan Umasankarkumudhini <shreedhushandhan.umasankarkumudhini@cesltd.com>
2021-09-03 21:09:06 +0530

    Login and Signin Model updated, Card-Pay Section Added, Carousel Bug Fixed

---
+++ script/loginModel.js
@@ -0,0 +1,13 @@
+let loginButton = document.getElementById('loginBtn')
+let loginModeId = document.getElementById('loginModelId')
+let signinId = document.getElementById('signinModelId')
+let closeBtn = document.getElementById('closeBtnId')
+
+function loginModel() {
+    loginModeId.style.display = 'block'
+    loginModeId.style.display = 'flex'
+}
+
+function closeModel() {
+    loginModeId.style.display = 'none'
+}
