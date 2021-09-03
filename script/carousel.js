8f96c44367ed6d9ffba3870509796a485a95359d
Shreedhushandhan Umasankarkumudhini <shreedhushandhan.umasankarkumudhini@cesltd.com>
2021-09-03 20:14:50 +0530

    Deals Update, Carousel, login Model, CSS Animations

--- script/carousel.js
+++ script/carousel.js
@@ -1,16 +1,46 @@
-let slideIndex = 1
-carousel()
-function carousel() {
-    let slides = document.getElementById('slides')
+let prevButton = document.getElementById('prevBtn')
+let nextButton = document.getElementById('nextBtn')
 
-    if(slideIndex > slides.length) {
-        slideIndex = 1
-    }
+let slides = document.getElementById('slidesId')
 
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"
+let matrixVal = new WebKitCSSMatrix(slides.transform)
+
+let pos = matrixVal.m41
+
+console.log(pos)
+
+prevButton.addEventListener('click', () => {
+    console.log(pos)
+    if(pos == 0) {
+        break    
+    } else if(pos > 0 && pos < -1345 ) {
+        slides.style.transform = "translateX(0)"    
+    } else if (pos > -1345 && pos < -2690) {
+        slides.style.transform = "translateX(-1345px)"
+    } else if (pos > -2690 && pos < -4035) {
+        slides.style.transform = "translateX(-2690px)"
+    } else if (pos > -4035 && pos < -5380) {
+        slides.style.transform = "translateX(-4035px)"
+    } else if (pos > -5380) {
+        slides.style.transform = "translateX(0)"
     }
-    slideIndex++
-    slides[slideIndex - 1].style.display = "block"
-    setTimeout(carousel, 3000)
+    // slides.style.transform = "translateX(1345px)"
+})
+
+function next() {
+    console.log(pos)
+    if(pos == 0) {
+        slides.style.transform = "translateX(-1345)"
+    } else if(pos < 0 && pos > -1345 ) {
+        slides.style.transform = "translateX(0)"    
+    } else if (pos > -1345 && pos < -2690) {
+        slides.style.transform = "translateX(-1345px)"
+    } else if (pos > -2690 && pos < -4035) {
+        slides.style.transform = "translateX(-2690px)"
+    } else if (pos > -4035 && pos < -5380) {
+        slides.style.transform = "translateX(-4035px)"
+    } else if (pos > -5380) {
+        slides.style.transform = "translateX(0)"
+    }
+    // slides.style.transform = "translateX(1345px)"
 }
